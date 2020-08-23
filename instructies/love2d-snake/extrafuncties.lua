6dc0a55311e6203b58186893f76b7407d01abeaf
bashopman <bashopman@users.noreply.github.com>
2020-08-23 20:08:41 +0000

    Publishing Site coderdojo-nijmegen.nl at 147f68e7794c14e2a48dce450fab8b89228e7e18 on Sun Aug 23 20:08:41 UTC 2020

---
+++ instructies/love2d-snake/extrafuncties.lua
@@ -0,0 +1,29 @@
+local extrafuncties = {}
+
+-- controleer of het keerpunt nog niet bestaat
+function extrafuncties.bestaatKeerPunt(keerPunten, keerPunt)
+  bestaat = false
+  for k, kp in pairs(keerPunten) do
+    if keerPunt.positieX == kp.positieX and keerPunt.positieY == kp.positieY then
+      bestaat = true
+    end
+  end
+  return bestaat
+end
+
+-- verwijder het eerste keerpunt als deze niet meer gebruikt wordt door de slang
+function extrafuncties.controleerKeerPunten(keerPunten, slang)
+  for kid, keerPunt in pairs(keerPunten) do
+    test = nil
+    for sid, slangStukje in pairs(slang) do
+      if slangStukje.positieX == keerPunt.positieX and slangStukje.positieY == keerPunt.positieY then
+        test = kid
+      end
+    end
+    if test == nil then
+      table.remove(keerPunten, 1)
+    end
+  end
+end
+
+return extrafuncties
