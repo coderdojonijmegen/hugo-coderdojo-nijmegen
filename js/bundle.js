ae75f64447d9ede8471ca659365c17e419741a8e
bashopman <bashopman@users.noreply.github.com>
2020-08-23 19:47:59 +0000

    Publishing Site coderdojo-nijmegen.nl at b0f67b76d54150e1f332179882d46797a763c273 on Sun Aug 23 19:47:59 UTC 2020

--- js/bundle.js
+++ js/bundle.js
@@ -1,3 +1,39 @@
+let mainNav = document.getElementById("js-menu");
+let navBarToggle = document.getElementById("js-menu-toggle");
+
+navBarToggle.addEventListener("click", function() {
+  mainNav.classList.toggle("active");
+});
+;
+var show = function(elem) {
+    elem.style.display = "block";
+}
+
+var hide = function(elem) {
+    elem.style.display = "none";
+}
+
+var toggle = function(elem) {
+    if (window.getComputedStyle(elem).display == "block") {
+        hide(elem);
+    } else {
+        show(elem);
+    }
+}
+
+document.addEventListener('click', function(event) {
+    if (!event.target.classList.contains("toggle_header")) return;
+    var parentElement = event.target.parentElement;
+    var parentElementChildren = parentElement.children;
+    for (var i = 0; i < parentElementChildren.length; i ++) {
+        var childElement = parentElementChildren[i];
+        if (!childElement.classList.contains("toggle_header")) {
+            toggle(childElement);
+        }
+    }
+}, false);
+
+;
 /**
  * scratchblocks v3.4.0
  * https://scratchblocks.github.io/
