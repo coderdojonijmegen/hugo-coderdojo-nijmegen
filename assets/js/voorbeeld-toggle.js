65df34a518dae21ccb3226679ad091b88facbbaa
Bas Hopman <bas@familiehopman.net>
2020-10-08 21:55:15 +0200

    toggle driehoekje van de voorbeelden gefixed. Scratch layout gefixed.

--- assets/js/voorbeeld-toggle.js
+++ assets/js/voorbeeld-toggle.js
@@ -15,13 +15,20 @@ var toggle = function(elem) {
 }
 
 document.addEventListener('click', function(event) {
-    if (!event.target.classList.contains("toggle_header")) return;
+    if (!(event.target.classList.contains("toggle_header") || event.target.classList.contains("toggle_header_open"))) return;
     var parentElement = event.target.parentElement;
     var parentElementChildren = parentElement.children;
     for (var i = 0; i < parentElementChildren.length; i ++) {
         var childElement = parentElementChildren[i];
-        if (!childElement.classList.contains("toggle_header")) {
+        if (!(childElement.classList.contains("toggle_header") || childElement.classList.contains("toggle_header_open"))) {
             toggle(childElement);
         }
+    }
+    if (event.target.classList.contains("toggle_header")) {
+        event.target.classList.remove("toggle_header");
+        event.target.classList.add("toggle_header_open");
+    } else {
+        event.target.classList.remove("toggle_header_open");
+        event.target.classList.add("toggle_header");
     }
 }, false);
