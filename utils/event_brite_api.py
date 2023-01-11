f8208a547d9bb73655f02343ac32a3d0f88e5bee
Bas Hopman <bas@familiehopman.net>
2023-01-11 21:52:40 +0100

    event_brite_api.py bijgewerkt zodat de output een Markdown tabel is; kopiëren in een .md bestand, previewen en je kunt de geformateerde tabel in een (Google) doc plakken

--- utils/event_brite_api.py
+++ utils/event_brite_api.py
@@ -6,7 +6,7 @@ from requests import get
 # documentation: https://www.eventbrite.nl/platform/api
 # new API key from: https://www.eventbrite.nl/platform/api-keys/
 #   note: each visit of this page creates a new API key at https://www.eventbrite.nl/account-settings/apps
-
+# API key stored in Bitwarden vault Bas
 auth_header = {"Authorization": "Bearer <api-key>"}
 
 events = []
@@ -20,7 +20,7 @@ while has_more_items:
     for event_json in events_json["events"]:
         start = datetime.strptime(event_json["start"]["local"], "%Y-%m-%dT%H:%M:%S")
         event_json["event_date"] = start
-        if start.year == 2021:
+        if start.year == 2022:
             ra = get(f"https://www.eventbriteapi.com/v3/events/{event_json['id']}/attendees", headers=auth_header)
             event_json['attendees'] = ra.json()
             rd = get(f"https://www.eventbriteapi.com/v3/events/{event_json['id']}/description", headers=auth_header)
@@ -37,7 +37,9 @@ def get_checkins(event):
     return len([attendee for attendee in event['attendees']['attendees'] if attendee['checked_in']])
 
 
-print("maand - locatie - onderwerp - aantal tickets, gekocht, ingecheckt")
+print("|maand | locatie | onderwerp | aantal tickets | gekocht | ingecheckt |")
+print("|------|---------|-----------|----------------|---------|------------|")
 for event in events:
-    print(f"{datetime.strftime(event['event_date'], '%m')} - {'online' if event['online_event'] else event['venue']['name']} - {event['name']['text']} - {event['capacity']}, {get_sold_tickets(event)}, {get_checkins(event)}")
+    print(f"|{datetime.strftime(event['event_date'], '%m')} | {'online' if event['online_event'] else event['venue']['name']} "
+          f"| {event['name']['text']} | {event['capacity']}| {get_sold_tickets(event)}| {get_checkins(event)}|")
 
